c2d076e6cb05d7519d0751b90959430582ededfe
marko <markoffka.kuznetsov@mail.ru>
2024-11-04 20:10:36 +0700

    final

--- MapMap.java
+++ MapMap.java
@@ -11,9 +11,9 @@ public class MapMap {
 
     public static void main(String[] args) {
         Map<String, Integer> originalMap = new HashMap<>();
-        originalMap.put("Alice", 30);
-        originalMap.put("Bob", 25);
-        originalMap.put("Charlie", 35);
+        originalMap.put("Marko", 53);
+        originalMap.put("Korvin", 10);
+        originalMap.put("Li Ran", 45);
 
         Map<Integer, String> invertedMap = invertMap(originalMap);
         System.out.println("Инвертированная Map: " + invertedMap);
